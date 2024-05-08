10ce5eae7c28acf9c4853ae61be9e22ec1354e43
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-08 12:42:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function isSameType(value1, value2) {
   //your js code here
-	// if (isNaN(value1) || isNaN(value2)) {
- //    return false;
- //  } 
-  // Then compare types using typeof
+  if (isNaN(value1) && isNaN(value2)) {
+    return true;
+  }
+  // Then compare types using typeof (excluding NaN)
   return typeof value1 === typeof value2;
 	
 }
